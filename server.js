de84e0333c61224bd942491fb14dafc5ef2f9fd1
Isaiah Henson <Devpuddles@gmail.com>
2022-07-13 23:59:21 -0400

    completed connection to the database and add ejs files

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
+//Decleared Variables
 const express = require('express')
 const app = express()
 const PORT = 8500
 const mongoose = require('mongoose')
+require('dotenv').config()
+//add model variable
+
+//Middleware
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect(process.env.DB_CONNECTION,
+    { useNewUrlParser: true },
+    () => { console.log('Connected to db!')}
+)
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT} better go catch it!`))
