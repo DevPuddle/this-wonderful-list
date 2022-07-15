f6e826a94003c51d8bb254e07e0f213220740319
Isaiah Henson <Devpuddles@gmail.com>
2022-07-15 00:51:35 -0400

    exported schema

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express')
 const app = express()
 const PORT = 8500
 const mongoose = require('mongoose')
+const todotasks = require('./models/todotasks')
 require('dotenv').config()
-//add model variable
-
+const TodoTask = require('./models/TodoTasks')
 //Middleware
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -14,6 +14,19 @@ app.use(express.urlencoded({ extended: true }))
 mongoose.connect(process.env.DB_CONNECTION,
     { useNewUrlParser: true },
     () => { console.log('Connected to db!')}
+)
+
+app.get('/', async (req, res) => {
+    try{
+        TodoTasks.find((), (err, tasks) => {
+            res.render('index.ejs', {todotasks: tasks})
+        })
+    } catch (err) {
+
+    }
+
+}
+
 )
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT} better go catch it!`))
